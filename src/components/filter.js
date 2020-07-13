22da8ccc2266f4f20aac067a7be632a2aa86cf2e
Albert <zubov.warrior@gmail.com>
2020-07-13 08:14:06 +0300

    Учебный проект: шаблонизируй это(final2)

--- src/components/filter.js
+++ src/components/filter.js
@@ -2,14 +2,14 @@ export const createFilter = (data) => `
   <section class="main__filter filter container">
     ${data
       .map(
-        (elem, ind) => `
+        (elem) => `
       <input
           type="radio"
           id="filter__${elem.title.toLowerCase()}"
           class="filter__input visually-hidden"
           name="filter"
-          ${elem.isActive ? elem.isActive : ``}
-          ${elem.isDisabled ? elem.isDisabled : ``}
+          ${elem.isActive === true ? `checked` : ``}
+          ${elem.isDisabled === true || elem.count <= 0 ? `disabled` : ``}
         />
         <label for="filter__${elem.title.toLowerCase()}" class="filter__label">
           ${elem.title} <span class="filter__all-count">${
