c132437403d2f535031fef57b23cf6df024d1159
Albert <42963174+albertZubov@users.noreply.github.com>
2020-07-13 16:44:10 +0300

    Module1 task2 (#6)
    * Время разбивать камни (разбиение main.js на модули
    
    * Учебный проект: шаблонизируй это
    
    * Учебный проект: шаблонизируй это (финал)
    
    * Учебный проект: шаблонизируй это(финал[доработки])
    
    * Учебный проект: шаблонизируй это(завершение)
    
    * Учебный проект: шаблонизируй это(final)
    
    * Auto stash before merge of "module1-task2" and "origin/module1-task2"
    
    * Учебный проект: шаблонизируй это(final2)
    
    * Учебный проект: шаблонизируй это(final3)

---
+++ src/components/filter.js
@@ -0,0 +1,23 @@
+export const createFilter = (data) => `
+  <section class="main__filter filter container">
+    ${data
+      .map(
+        (elem) => `
+      <input
+          type="radio"
+          id="filter__${elem.title.toLowerCase()}"
+          class="filter__input visually-hidden"
+          name="filter"
+          ${elem.isActive ? `checked` : ``}
+          ${elem.isDisabled || elem.count <= 0 ? `disabled` : ``}
+        />
+        <label for="filter__${elem.title.toLowerCase()}" class="filter__label">
+          ${elem.title} <span class="filter__all-count">${
+          elem.count
+        }</span></label
+      >
+    `
+      )
+      .join(``)}
+  </section>
+`;
